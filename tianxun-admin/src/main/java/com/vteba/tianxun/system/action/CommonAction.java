b3a58ca14a1ac04e7ccc90943ad4db4fd0e70d7e
yinlei <tongku2008@126.com>
2016-04-03 13:24:04 +0800

    登录页面

--- tianxun-admin/src/main/java/com/vteba/tianxun/system/action/CommonAction.java
+++ tianxun-admin/src/main/java/com/vteba/tianxun/system/action/CommonAction.java
@@ -3,6 +3,7 @@ package com.vteba.tianxun.system.action;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.net.URLEncoder;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletResponse;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.vteba.common.constant.CommonConst;
 import com.vteba.tianxun.user.model.User;
+import com.vteba.tianxun.utils.common.UserUtils;
 import com.vteba.utils.common.RandomNumber;
 import com.vteba.web.action.GenericAction;
 
@@ -23,6 +25,45 @@ import com.vteba.web.action.GenericAction;
 @Controller
 @RequestMapping("/common")
 public class CommonAction extends GenericAction<User> {
+	
+	@RequestMapping("/platform")
+	public String platofrm() {
+		User user = UserUtils.getUser();
+		if (user != null) {
+			setAttributeToSession(CommonConst.CONTEXT_USER, user);
+			
+			// 登录成功，将账号和机构代码，放入cookie中
+			// 集群状态下同步用户信息是需要的，将状态放在客户端
+			String account = "";
+			try {
+				account = URLEncoder.encode(user.getAccount(), "UTF-8");
+			} catch (UnsupportedEncodingException e) {
+				// ignore
+			}
+			Cookie cookie = new Cookie(CommonConst.SEC_ACCOUNT, account);
+			cookie.setMaxAge(Integer.MAX_VALUE);
+			cookie.setPath("/");
+			getHttpServletResponse().addCookie(cookie);
+			
+//			cookie = new Cookie(CommonConst.SEC_TENANT, user.getOrgCode());
+//			cookie.setMaxAge(Integer.MAX_VALUE);
+//			cookie.setPath("/");
+//			getHttpServletResponse().addCookie(cookie);
+//			// 如果有首页的统计权限，显示统计
+//			DefaultGrantedAuthority authority = new DefaultGrantedAuthority("平台总览", user.getOrgId());
+//			if (user.getAuthorities().contains(authority)) {
+//				setAttributeToSession(INDEX_STATS, true);
+//				return "redirect:/indexStats/show";
+//			} else {
+//				// 没有统计权限，显示普通页
+//				setAttributeToSession(INDEX_STATS, false);
+//				return "common/platform";
+//			}
+			return "common/platform";
+		} else { // 没有获取到用户信息，调到登录页
+			return "redirect:/employee/login";
+		}
+	}
 	
 	@RequestMapping("/sessionTimeout")
 	public String sessionTimeout() {
